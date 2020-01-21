ee070acd1ac4c1f1c13a4f4bdecab533fa18fd85
Patryk <patryk.wlaz@gmail.com>
2020-01-21 13:11:22 +0100

    setup-project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainViewComponent } from './main-view/main-view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
