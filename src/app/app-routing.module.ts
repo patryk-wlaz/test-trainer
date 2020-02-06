2746ac3ead1b743deec0fb47e2a4580e49d67135
Patryk <patryk.wlaz@gmail.com>
2020-02-06 15:14:04 +0100

    fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { map } from 'lodash-es';
 import { MainViewComponent } from './main-view/main-view.component';
 import { config } from './config';
 import { TestComponent } from './test/test.component';
 
-const testRoutes = map(config, test => ({
-  path: test.route,
-  component: TestComponent,
-  data: { test },
-}));
+/*
+  When I try to dynamically create routes based on config, I see error "Cannot read property 'loadChildren' of undefined"
+  It looks like an angular error, whichc was supposed to be solved in previous version of framework.
+  I have no time to fix this now, so when adding new tests you need to add routes manually.
+*/
+
+// const testRoutes = map(config, test => ({
+//   path: test.route,
+//   component: TestComponent,
+//   data: { test },
+// }));
 
 const routes: Routes = [
-  ...testRoutes,
+  // ...testRoutes,
+  {
+    path: config[0].route,
+    component: TestComponent,
+    data: config[0],
+  },
   {
     path: '**',
     component: MainViewComponent,
